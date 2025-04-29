866bb92f4f50c950b5e17b02aa74f29a6d65f100
osamasilvake@gmail.com <muhammadosamavu@gmail.com>
2025-04-29 22:27:54 +0500

    refactor: Code

--- src/app/components/features-location/FeaturesLocation.tsx
+++ src/app/components/features-location/FeaturesLocation.tsx
@@ -6,13 +6,13 @@ const FeaturesLocation = () => {
       className="container mx-auto bg-gradient-to-r from-regal-blue  via-purple-800
       to-red-500"
     >
-      <div className="flex flex-col md:flex-row gap-16 md:items-center px-20 pt-8 pb-10">
-        <div className="md:w-[75%]">
+      <div className="flex flex-col-reverse md:flex-row gap-1 md:gap-16 justify-center items-center px-8 md:px-20 pt-8 pb-10">
+        <div className="md:w-[75%] flex flex-col items-center md:items-start">
           <h2 className="text-3xl font-[900] py-1 text-white">
             Grand Opening
             <span className="text-[#FDF200] font-[700]"> Sale</span>
           </h2>
-          <p className="text-2xl font-[900] text-white pb-6">
+          <p className="text-xl md:text-2xl font-[900] text-white pb-6 text-center md:text-left">
             With 167 new Best Buy Express locations, it's easier than ever to
             get your hands on the latest tech.
           </p>
@@ -26,7 +26,7 @@ const FeaturesLocation = () => {
             src="https://picsum.photos/100?random=6"
             width={180}
             height={160}
-            className="min-w-40 w-full md:w-48"
+            className="min-w-40 md:w-48"
           />
         </div>
       </div>
